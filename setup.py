ec53ac04a322d8fdeca62cb957ed5da13246841c
Kory Mathewson <korymath@gmail.com>
2019-02-12 11:00:32 -0700

    updating the setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 from setuptools import find_packages
 
+
 setup(
     name="Jann",
+    verson="0.0.1",
+    description="Jann is a Nearest Neighbour retrieval-based chatbot.",
+    author="Kory Mathewson",
+    author_email="korymath@gmail.com",
+    license="MIT",
+    url="https://github.com/korymath/jann",
     packages=find_packages(),
     setup_requires=[
         "pytest-runner"
