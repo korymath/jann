aeb3998394ed7567e5d8bb5668003dd8e77051ea
Kory Mathewson <korymath@gmail.com>
2019-02-12 10:42:06 -0700

    moving bash run examples into module

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-export TFHUB_CACHE_DIR=data/module
+export TFHUB_CACHE_DIR=Jann/data/module
 
 mkdir data/module
 
